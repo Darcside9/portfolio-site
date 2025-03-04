8c376a90586c556b8f192a6ab86cfc172f9b2afd
D∆RC <darcside999@gmail.com>
2025-03-04 13:37:03 +0100

    eslint deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import { rules } from "eslint-config-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,8 +14,11 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "next", "prettier"],
     rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
+      ...prettierConfig.rules, // Ensure Prettier rules are properly applied
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "@typescript-eslint/no-unused-vars": "off", //disabled unused variables globally
+      "@typescript-eslint/no-explicit-any": "off" //disable "any" warnings
     },
   }),
 ];
